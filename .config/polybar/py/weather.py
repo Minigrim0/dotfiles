30adfa535df4042479de045205dd8bbd8ab1d69d
Minigrim0 <grimauflorent@gmail.com>
2021-07-22 16:41:00 +0200

    [Upd] file paths

--- .config/polybar/py/weather.py
+++ .config/polybar/py/weather.py
@@ -1,34 +1,33 @@
+import fontawesome as fa
+
 import requests
 import json
 
-try:
-    with open('.env.json') as key:
-        API_KEY = json.load(key).get("OPENWEATHER_KEY", None)
+with open('~/.config/polybar/py.env.json') as key:
+    API_KEY = json.load(key).get("OPENWEATHER_KEY", None)
 
-    if API_KEY is None:
-        print("No key found")
-        exit(0)
+if API_KEY is None:
+    print("No key found")
+    exit(0)
 
-    API_URL = "https://api.openweathermap.org/data/2.5/weather?q=brussels&appid={}".format(API_KEY)
+API_URL = "https://api.openweathermap.org/data/2.5/weather?q=brussels&appid={}".format(API_KEY)
 
-    r = requests.get(API_URL)
-    data = json.loads(r.content)
+r = requests.get(API_URL)
+data = json.loads(r.content)
 
-    weather_icons = {
-        "01": "",
-        "02": "",
-        "03": "",
-        "04": "",
-        "O9": "",
-        "10": "",
-        "11": "",
-        "13": "",
-        "50": "",
-    }
+weather_icons = {
+    "01": "",
+    "02": "",
+    "03": "",
+    "04": "",
+    "O9": "",
+    "10": "",
+    "11": "",
+    "13": "",
+    "50": "",
+}
 
-    weather_icon = data["weather"][0]['icon'][:-1]
-    temperature = data["main"]['temp'] - 273.15
+weather_icon = data["weather"][0]['icon'][:-1]
+temperature = data["main"]['temp'] - 273.15
 
-    print("{}  {}°C".format(weather_icons[weather_icon], round(temperature, 1)))
-except Exception as e:
-    print(e)
+print("{} {}°C".format(weather_icons[weather_icon], round(temperature, 1)))
