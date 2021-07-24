be33a977d2649ae8390364f9af0f5f5c2677bb74
Minigrim0 <grimauflorent@gmail.com>
2021-07-24 15:33:28 +0200

    [Upd] powermenu

--- .config/polybar/scripts/powermenu.sh
+++ .config/polybar/scripts/powermenu.sh
@@ -7,24 +7,10 @@ rofi_command="rofi -theme $dir/powermenu.rasi"
 
 # Options
 shutdown=" Shutdown"
-reboot=" Restart"
+reboot=" Reboot"
 lock=" Lock"
 suspend=" Sleep"
 logout=" Logout"
-
-# Confirmation
-confirm_exit() {
-    rofi -dmenu\
-        -i\
-        -no-fixed-num-lines\
-        -p "Are You Sure? : "\
-        -theme $dir/confirm.rasi
-}
-
-# Message
-msg() {
-    rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
-}
 
 # Variable passed to rofi
 options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
@@ -32,24 +18,10 @@ options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
 chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
 case $chosen in
     $shutdown)
-        ans=$(confirm_exit &)
-        if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-            systemctl poweroff
-        elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-            exit 0
-        else
-            msg
-        fi
+        systemctl poweroff
         ;;
     $reboot)
-        ans=$(confirm_exit &)
-        if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-            systemctl reboot
-        elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-            exit 0
-        else
-            msg
-        fi
+        systemctl reboot
         ;;
     $lock)
         if [[ -f /usr/bin/i3lock-fancy-rapid ]]; then
@@ -59,27 +31,12 @@ case $chosen in
         fi
         ;;
     $suspend)
-        ans=$(confirm_exit &)
-        if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-            mpc -q pause
-            amixer set Master mute
-            systemctl suspend
-        elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-            exit 0
-        else
-            msg
-        fi
+        amixer set Master mute
+        systemctl suspend
         ;;
     $logout)
-        ans=$(confirm_exit &)
-        if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-            if [[ "$DESKTOP_SESSION" == "i3" ]]; then
-                i3-msg exit
-            fi
-        elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-            exit 0
-        else
-            msg
+        if [[ "$DESKTOP_SESSION" == "i3" ]]; then
+            i3-msg exit
         fi
         ;;
 esac
