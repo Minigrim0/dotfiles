8f13f1264bd49b3d0ae1b27ea60074cf8fc05129
Minigrim0 <grimauflorent@gmail.com>
2021-07-27 15:49:33 +0200

    [Upd] show errors

--- .config/polybar/py/weather.py
+++ .config/polybar/py/weather.py
@@ -4,33 +4,36 @@ from hereby import Here
 import requests
 import json
 
-here = Here(__file__)
+try:
+    here = Here(__file__)
 
-with open(here.abspath('.env.json')) as key:
-    API_KEY = json.load(key).get("OPENWEATHER_KEY", None)
+    with open(here.abspath('.env.json')) as key:
+        API_KEY = json.load(key).get("OPENWEATHER_KEY", None)
 
-if API_KEY is None:
-    print("No key found")
-    exit(0)
+    if API_KEY is None:
+        print("No key found")
+        exit(0)
 
-API_URL = "https://api.openweathermap.org/data/2.5/weather?q=brussels&appid={}".format(API_KEY)
+    API_URL = "https://api.openweathermap.org/data/2.5/weather?q=brussels&appid={}".format(API_KEY)
 
-r = requests.get(API_URL)
-data = json.loads(r.content)
+    r = requests.get(API_URL)
+    data = json.loads(r.content)
 
-weather_icons = {
-    "01": "",
-    "02": "",
-    "03": "",
-    "04": "",
-    "O9": "",
-    "10": "",
-    "11": "",
-    "13": "",
-    "50": "",
-}
+    weather_icons = {
+        "01": "sun",
+        "02": "cloud-sun",
+        "03": "cloud",
+        "04": "cloud",
+        "O9": "cloud-rain",
+        "10": "cloud-sun-rain",
+        "11": "bolt",
+        "13": "snowflake",
+        "50": "fog",
+    }
 
-weather_icon = data["weather"][0]['icon'][:-1]
-temperature = data["main"]['temp'] - 273.15
+    weather_icon = data["weather"][0]['icon'][:-1]
+    temperature = data["main"]['temp'] - 273.15
 
-print("{} {}°C".format(weather_icons[weather_icon], round(temperature, 1)))
+    print(f"{fa.icons[weather_icons[weather_icon]]} {round(temperature, 1)}°C")
+except Exception as e:
+    print(e)
