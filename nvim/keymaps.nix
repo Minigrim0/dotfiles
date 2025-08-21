2bd3e185ebc04a695036219d9b3e83e5a6c7873b
Minigrim0 <fgrimau@proton.me>
2025-08-21 18:34:41 +0200

    Add window navigation with Ctrl+hjkl and Ctrl+arrows

--- nvim/keymaps.nix
+++ nvim/keymaps.nix
@@ -43,6 +43,58 @@
       options.desc = "Explorer (snacks)";
     }
     
+    # Window navigation with Ctrl + hjkl
+    {
+      mode = "n";
+      key = "<C-h>";
+      action = "<C-w>h";
+      options.desc = "Go to left window";
+    }
+    {
+      mode = "n";
+      key = "<C-j>";
+      action = "<C-w>j";
+      options.desc = "Go to lower window";
+    }
+    {
+      mode = "n";
+      key = "<C-k>";
+      action = "<C-w>k";
+      options.desc = "Go to upper window";
+    }
+    {
+      mode = "n";
+      key = "<C-l>";
+      action = "<C-w>l";
+      options.desc = "Go to right window";
+    }
+
+    # Window navigation with Ctrl + arrow keys
+    {
+      mode = "n";
+      key = "<C-Left>";
+      action = "<C-w>h";
+      options.desc = "Go to left window";
+    }
+    {
+      mode = "n";
+      key = "<C-Down>";
+      action = "<C-w>j";
+      options.desc = "Go to lower window";
+    }
+    {
+      mode = "n";
+      key = "<C-Up>";
+      action = "<C-w>k";
+      options.desc = "Go to upper window";
+    }
+    {
+      mode = "n";
+      key = "<C-Right>";
+      action = "<C-w>l";
+      options.desc = "Go to right window";
+    }
+
     # Terminal toggle (from your toggleterm config)
     {
       mode = "n";
